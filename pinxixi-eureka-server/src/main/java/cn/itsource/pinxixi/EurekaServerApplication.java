d6d4dca08aad7b2b4b841c69286afc59b4acda98
黄海 <280904958@qq.com>
2019-05-15 21:34:14 +0800

    修改

--- pinxixi-eureka-server/src/main/java/cn/itsource/pinxixi/EurekaServerApplication.java
+++ pinxixi-eureka-server/src/main/java/cn/itsource/pinxixi/EurekaServerApplication.java
@@ -1,9 +1,10 @@
 package cn.itsource.pinxixi;
 
 import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
 import org.springframework.cloud.netflix.eureka.server.EnableEurekaServer;
 
-@springBootApplication
+@SpringBootApplication
 @EnableEurekaServer
 public class EurekaServerApplication {
     public static void main(String[] args) {
