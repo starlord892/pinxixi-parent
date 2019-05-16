9d6bcb52dd4a9fea2c932e07d4d595b21c4fea3a
黄海 <280904958@qq.com>
2019-05-16 19:24:46 +0800

    修改项目代码bug

--- pinxixi-plat-parent/plat-interface/src/main/java/cn/itsource/pinxixi/domain/Employee.java
+++ pinxixi-plat-parent/plat-interface/src/main/java/cn/itsource/pinxixi/domain/Employee.java
@@ -17,24 +17,26 @@ public class Employee implements Serializable {
 
     private String username;
 
-    private String realname;
+
 
     private String password;
 
-    private String tel;
+
 
     private String email;
 
-    private Long deptId;
+    private Long department_id;
 
-    private LocalDate inputtime;
 
-    private Integer state;
 
-    private String unionId;
+    private Integer age;
 
-    private Long tenantId;
+    private String headimage;
 
+
+    public static long getSerialVersionUID() {
+        return serialVersionUID;
+    }
 
     public Long getId() {
         return id;
@@ -52,28 +54,12 @@ public class Employee implements Serializable {
         this.username = username;
     }
 
-    public String getRealname() {
-        return realname;
-    }
-
-    public void setRealname(String realname) {
-        this.realname = realname;
-    }
-
     public String getPassword() {
         return password;
     }
 
     public void setPassword(String password) {
         this.password = password;
-    }
-
-    public String getTel() {
-        return tel;
-    }
-
-    public void setTel(String tel) {
-        this.tel = tel;
     }
 
     public String getEmail() {
@@ -84,60 +70,40 @@ public class Employee implements Serializable {
         this.email = email;
     }
 
-    public Long getDeptId() {
-        return deptId;
+    public Long getDepartment_id() {
+        return department_id;
     }
 
-    public void setDeptId(Long deptId) {
-        this.deptId = deptId;
+    public void setDepartment_id(Long department_id) {
+        this.department_id = department_id;
     }
 
-    public LocalDate getInputtime() {
-        return inputtime;
+    public Integer getAge() {
+        return age;
     }
 
-    public void setInputtime(LocalDate inputtime) {
-        this.inputtime = inputtime;
+    public void setAge(Integer age) {
+        this.age = age;
     }
 
-    public Integer getState() {
-        return state;
+    public String getHeadimage() {
+        return headimage;
     }
 
-    public void setState(Integer state) {
-        this.state = state;
-    }
-
-    public String getUnionId() {
-        return unionId;
-    }
-
-    public void setUnionId(String unionId) {
-        this.unionId = unionId;
-    }
-
-    public Long getTenantId() {
-        return tenantId;
-    }
-
-    public void setTenantId(Long tenantId) {
-        this.tenantId = tenantId;
+    public void setHeadimage(String headimage) {
+        this.headimage = headimage;
     }
 
     @Override
     public String toString() {
         return "Employee{" +
                 "id=" + id +
-                ", username=" + username +
-                ", realname=" + realname +
-                ", password=" + password +
-                ", tel=" + tel +
-                ", email=" + email +
-                ", deptId=" + deptId +
-                ", inputtime=" + inputtime +
-                ", state=" + state +
-                ", unionId=" + unionId +
-                ", tenantId=" + tenantId +
-                "}";
+                ", username='" + username + '\'' +
+                ", password='" + password + '\'' +
+                ", email='" + email + '\'' +
+                ", department_id=" + department_id +
+                ", age=" + age +
+                ", headImage='" + headimage + '\'' +
+                '}';
     }
 }
